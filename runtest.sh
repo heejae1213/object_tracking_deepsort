30bd511965728c51d23151cc9c6cee930a88d6f2
Hazel Han <heejae941213@gmail.com>
2021-01-25 19:29:11 -0500

    Delete unnecessary files

--- runtest.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-python "$PWD"/test.py
-
--- runtracker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "Enter the video path"
-read vpath
-python /home/cc/nanonets_object_tracking/detection/yolo.py --play_video "True" --video_path "$vpath"
-echo "Detection Done"
-echo "Start Tracking..."
-python /home/cc/nanonets_object_tracking/test_on_video.py --video_path "$vpath"
